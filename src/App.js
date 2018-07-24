6e033e4b59edd79a365f91d2bfa9026bde64cee8
Romario Herbert <romarioh39@gmail.com>
2018-07-23 18:03:42 -0600

    modal complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import CSSModules from 'react-css-modules'
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
-import Modal from './modal'
+import css from './index.css'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-      this.state = false
-  }
+window.onload=function(){
+  document.getElementById('button').addEventListener('click',
+  function() {
+    document.querySelector('.herb-modal').style.display = 'flex';
+  });
 
-  render() {
-    return (
-        <div className="App-intro">
-        <button onClick={this.toggleModal}>Open</button>
-        <Modal
-          isOpen={this.state.isModalOpen}
-          toggleModal={this.toggleModal}
-        />
-      </div>
-    );
-  }
+  document.querySelector('.close').addEventListener('click',
+    function() {
+      document.querySelector('.herb-modal').style.display = 'none';
+  });
 }
 
-export default App;
+class Modal extends Component {
+ render() {
+   return (
+     <main>
+      <section class="container">
+       <img src="3.jpg" alt="" />
+        <div class="text-content">
+          <h1>Let's create a modal</h1>
+          <h3>Sign Up Now</h3>
+          <a href="#" id="button" class="button">Click Me</a>
+        </div>
+      </section>
+      <div className="herb-modal">
+        <div className="modal-content">
+        <div class="close">+</div>
+        <img src="favicon.ico" alt="l" />
+        <form action="">
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="E-mail"/>
+          <a href="" class="button">Submit</a>
+        </form>
+        </div>
+      </div>
+     </main>
+   )
+ }
+}
+
+export default Modal
